1929026026239dd4de160f2d2cc1749ea05df20f
bhaveshAn <bhaveshanand7@gmail.com>
2018-01-25 01:09:12 +0530

    Fixes #91 Use requirements.txt to install dependencies in pip install

--- setup.py
+++ setup.py
@@ -4,15 +4,25 @@ try:
 except ImportError:
     from distutils.core import setup
 
+description = "Python API for loklak, Anonymous distributed P2P Systems."
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = description
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 setup(name='python-loklak-api',
       version='1.7',
-      description="Python API for loklak, Anonymous distributed P2P Systems.",
+      description=description,
+      long_description=long_description,
       author='Sudheesh Singanamalla',
       author_email='sudheesh95@gmail.com',
-      url='https://github.com/loklak/loklak_api_python',
+      url='https://github.com/loklak/loklak_python_api',
       license='',
+      include_package_data=True,
       platforms='Linux/Mac',
       py_modules=['loklak'],
+      python_requires='>=2.7.0',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
@@ -20,13 +30,10 @@ setup(name='python-loklak-api',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 3',
           'Topic :: Internet',
-          ],
-      keywords="Twitter Loklak Anonymous API",
-      install_requires=[
-        "requests==2.16.0",
-        "xmljson==0.1.9"
       ],
+      keywords="Twitter Loklak Anonymous API",
+      install_requires=requirements,
       zip_safe=False,
-      download_url='https://github.com/loklak/loklak_api_python',
+      download_url='https://github.com/loklak/loklak_python_api/archive/v1.7.tar.gz',
       scripts=['bin/loklak']
       )
